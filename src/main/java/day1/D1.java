87dd09f7a0e308443184b3049637dc4cb3518308
Sergej Hartfil <sergej.hartfil@sap.com>
2021-12-08 17:35:08 +0100

    intermediate HTTP request

--- src/main/java/day1/D1.java
+++ src/main/java/day1/D1.java
@@ -1,4 +1,12 @@
 package day1;
 
+import helper.DownloadData;
+
+import java.io.IOException;
+
 public class D1 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DownloadData downloadData = new DownloadData();
+        downloadData.downloadFile("test", 2016, 1);
+    }
 }
--- src/main/java/helper/DownloadData.java
+++ src/main/java/helper/DownloadData.java
@@ -1,17 +1,44 @@
 package helper;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class DownloadData {
-    public static void main(String[] args) {
+    public void downloadFile (String filename, int year, int day) throws IOException, InterruptedException {
+        // read the locally stored session cookie
         var fileReader = new FileReader();
         var sessionPath = "src/main/java/helper/Session";
         String sessionCookie;
         sessionCookie = fileReader.readFile(sessionPath);
 
+        // request the data from AoC
+        //URL url = new URL("https://adventofcode.com/" + year + "/" + day + "/input");
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://adventofcode.com/" + year + "/" + day + "/input"))
+                .setHeader("Cookie", "session=" + sessionCookie)
+                .build();
+
+        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        // Write a file with provided name and url
+        /*
         var fWriter = new FWriter();
         var dir = "src/main/resources/";
-        var filename = "test";
         fWriter.fWriter("test test\n test", dir, filename);
-
-
+        */
     }
+
+
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(5))
+            .build();
 }
