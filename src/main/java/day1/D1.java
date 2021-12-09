e988e78d75fc01e0fdce8dcf611eb2954e5b8490
shartf <shartf@users.noreply.github.com>
2021-12-09 22:48:22 +0100

    wip

--- src/main/java/day1/D1.java
+++ src/main/java/day1/D1.java
@@ -2,11 +2,31 @@ package day1;
 
 import helper.DownloadData;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class D1 {
+
     public static void main(String[] args) throws IOException, InterruptedException {
+        // download input
         DownloadData downloadData = new DownloadData();
-        downloadData.downloadFile("test", 2016, 1);
+        downloadData.downloadFile("day1", 2016, 1);
+
+        // split the input
+        Path path = Path.of("/src/main/resources/", "day1.txt");
+        if (Files.exists(path)) {
+                List<String> lines = Files.readAllLines(path);
+
+        }
     }
+
+
 }
