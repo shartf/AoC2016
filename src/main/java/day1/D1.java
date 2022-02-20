b39f7cc40ff0ddbbb69060b5705bfda3814c750b
shartf <shartf@users.noreply.github.com>
2022-02-20 12:48:46 +0100

    - finished day 1

--- src/main/java/day1/D1.java
+++ src/main/java/day1/D1.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class D1 {
 
@@ -18,13 +18,43 @@ public class D1 {
 
         // split the input
         Path path = Path.of("src/main/resources/", "day1.txt");
+        List<String[]> parsedList = null;
         if (Files.exists(path)) {
                 List<String> lines = Files.readAllLines(path);
-                List<String[]> parsed = lines.stream().map(s -> s.split(","))
-                        .collect(Collectors.toList());
-                System.out.println(parsed);
-
+                parsedList = lines.stream().map(s -> s.replaceAll("\\s", ""))
+                        .map(s -> s.split(","))
+                        .toList();
         }
+        assert parsedList != null;
+        String[] parsed =  parsedList.get(0);
+        // init arrayList
+        ArrayList<Cartesian> cartesianPath = new ArrayList<>();
+        cartesianPath.add(new Cartesian(0, 0, Directions.N));
+        // iterate over way description
+        assert parsed != null;
+        for (String strings : parsed) {
+            // get the next way description as string and the last Cartesian object as annealing point
+            cartesianPath.addAll(calculateDistance(strings, cartesianPath.get(cartesianPath.size() - 1)));
+        }
+        // end point
+        System.out.println(cartesianPath.get(cartesianPath.size()-1).toString());
+
+        for (int i = 1; i < cartesianPath.size() - 1; i++) {
+            Cartesian newCoord = cartesianPath.get(i);
+            // run through entries before the dumb way and search for same coordinates
+            for (int j = i - 1; j > 0; j--) {
+                Cartesian prevCoord = cartesianPath.get(j);
+                if (newCoord.x == prevCoord.x && newCoord.y == prevCoord.y) {
+                    System.out.println("Found ya! " + newCoord.toString());
+                    System.out.println("Number of blocks of path intersection is " + (
+                            Math.abs(newCoord.x) + Math.abs(newCoord.y)));
+                    break;
+                }
+            }
+        }
+
+
+
     }
 
     enum Directions {
@@ -70,10 +100,10 @@ public class D1 {
         // instantiate list of cartesians
         var cartList = new ArrayList<Cartesian>();
 
-        Turns turn = null;
+        Turns turn;
         turn = Turns.valueOf(String.valueOf(directionString.charAt(0)));
         int distance;
-        distance = Integer.parseInt(String.valueOf(directionString.charAt(1)));
+        distance = Integer.parseInt(directionString.substring(1));
         Directions newDirection = turnTo(lastCoordinate.dir, turn);
 
         switch (newDirection) {
